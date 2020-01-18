589022edb46fcc54f8c65b2a57c7778a16700ef7
yoyokazoo <peter.beckfield@gmail.com>
2020-01-17 22:20:02 -0800

    Poking around slack client

--- printDraftTiming.py
+++ printDraftTiming.py
@@ -1,1 +1,22 @@
-print("DraftTiming")
+import sys
+import os
+import argparse
+from ast import literal_eval
+import slack
+
+parser = argparse.ArgumentParser() 
+parser.add_argument('-token', '--token', help="Slack token of the channel you want to scan", required=True)
+args = parser.parse_args()
+print(args)
+
+client = slack.WebClient(token=args.token)
+
+print(client.channels_list())
+print(client.conversations_list())
+
+response = client.chat_postMessage(
+    channel='CS0FCKUKA',
+    text="Hello world!",
+    user='U106198RM')
+assert response["ok"]
+assert response["message"]["text"] == "Hello world!"
