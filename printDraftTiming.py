ac0c5fd18293083acaa7f88add5cbeff32391e45
yoyokazoo <peter.beckfield@gmail.com>
2020-01-19 19:48:07 -0800

    UserID Associations, return messages in time sorted order.

--- printDraftTiming.py
+++ printDraftTiming.py
@@ -4,35 +4,78 @@ import argparse
 from ast import literal_eval
 import slack
 
-parser = argparse.ArgumentParser() 
-parser.add_argument('-token', '--token', help="Workspace token of the app", required=True)
-parser.add_argument('-channel_id', '--channel_id', help="Channel ID of the channel you want to scan.  If not included, it will list the available channels and ids")
-args = parser.parse_args()
-print(args)
-
-client = slack.WebClient(token=args.token)
-
-if(not args.channel_id):
+def handleMissingChannelId(args, client):
+	if(not args.channel_id):
 		print("No channel ID specified.  Here are the available channels:")
 		channel_list = client.channels_list()
 		for channel in channel_list['channels']:
 			print(channel['name'] + " -- " + channel['id'])
 		exit()
 
-conversations = None
-while True:
-	if not conversations:
-		conversations = client.conversations_history(channel=args.channel_id)
-	else:
-		conversations = client.conversations_history(
-	  	channel=args.channel_id,
-	  	cursor=conversations['response_metadata']['next_cursor']
+def createUserAssociations(args, client):
+	username_to_uid = {}
+	uid_to_username = {}
+
+	channel_info = client.channels_info(
+  	channel=args.channel_id
+	)
+
+	for user in channel_info['channel']['members']:
+		# if we were dealing with more users, could probably grab these as a batch but meh
+		user_info = client.users_info(
+  		user=user
 		)
+		username = user_info['user']['name']
+		username_to_uid[username] = user
+		uid_to_username[user] = username
 
-	if not conversations['ok']:
-		break
+	for name in args.draft_order:
+		if not name in username_to_uid:
+			print("Couldn't find slack user '%s', exiting" % name)
+			exit()
 
-	print(str(conversations['ok']) + " -- " + str(conversations['has_more']) + " -- " + str(len(conversations['messages'])))
+	return username_to_uid, uid_to_username
 
-	if not conversations['has_more']:
-		break
+def getTimeOrderedMessages(args, client):
+	all_messages = []
+	conversations = None
+	while True:
+		if not conversations:
+			conversations = client.conversations_history(channel=args.channel_id)
+		else:
+			conversations = client.conversations_history(
+		  	channel=args.channel_id,
+		  	cursor=conversations['response_metadata']['next_cursor']
+			)
+
+		if not conversations['ok']:
+			break
+
+		#print(str(conversations['ok']) + " -- " + str(conversations['has_more']) + " -- " + str(len(conversations['messages'])))
+
+		all_messages.extend(conversations['messages'])
+
+		if not conversations['has_more']:
+			break
+
+		all_messages.sort(key=(lambda message: message['ts']))
+		return all_messages
+
+def printMessages(messages):
+	for message in messages:
+			if(message and message['text']):
+				print(message['user'] + " -- " + message['ts'] + " -- " + message['text'])
+
+parser = argparse.ArgumentParser() 
+parser.add_argument('-t', '--token', help="Workspace token of the app", required=True)
+parser.add_argument('-c', '--channel_id', help="Channel ID of the channel you want to scan.  If not included, it will list the available channels and ids")
+parser.add_argument('-d', '--draft_order', help="Order of drafters.  ", nargs='+')
+args = parser.parse_args()
+print(args)
+
+client = slack.WebClient(token=args.token)
+
+handleMissingChannelId(args, client)
+username_to_uid, uid_to_username = createUserAssociations(args,client)
+messages = getTimeOrderedMessages(args, client)
+printMessages(messages)
