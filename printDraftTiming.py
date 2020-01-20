b3b082247682a20e03037483e3b47f41e03d7c91
yoyokazoo <peter.beckfield@gmail.com>
2020-01-20 03:00:00 -0800

    Fixed first direction change bug.  Next issue is that it's not detecting Jesse Tagging peter on the way back from round 2.

--- printDraftTiming.py
+++ printDraftTiming.py
@@ -112,14 +112,16 @@ def getYoureUpNextMessages(args, client, messages, uid_to_username, uid_draft_or
 
 		#for tag_to_match in next_player_slack_tags[current_drafter_index][draft_key]:
 		tag_to_match = next_player_slack_tags[current_drafter_index][draft_key]
-		print("Checking %s against %s" % (tag_to_match, message['text']))
+		#print("Checking %s against %s" % (tag_to_match, message['text']))
 
 		# is it OK if someone else tags the person that's up? or should we check that the message sender is the previous drafter?
 		if tag_to_match in message['text']:
 			#print("%s %s %s", (messages[message_index-1]['text'], messages[message_index]['text'], messages[message_index+1]['text']))
 			prev_drafter_index = current_drafter_index
-			if draft_direction == 1 and current_drafter_index == len(uid_draft_order):
-				print("Done with round %s, changing direction" % current_draft_round)
+			prev_draft_round = current_draft_round
+
+			current_drafter_index = current_drafter_index + draft_direction
+			if (draft_direction == 1 and current_drafter_index == len(uid_draft_order) - 1) or (draft_direction == -1 and current_drafter_index == 0):
 				current_draft_round = current_draft_round + 1
 				if current_draft_round % 2 == 0:
 					draft_direction = -1
@@ -128,10 +130,10 @@ def getYoureUpNextMessages(args, client, messages, uid_to_username, uid_draft_or
 					draft_direction = 1
 					draft_key = 'next'
 
-			current_drafter_index = current_drafter_index + draft_direction
-
 			print("%s drafted at: %s (%s). Next drafter is: %s" % (uid_to_username[uid_draft_order[prev_drafter_index]], message['ts'], replaceUidWithUsername(messages[message_index]['text'].replace("\n", " "), uid_to_username), uid_to_username[uid_draft_order[current_drafter_index]]))
-			print("New tag to match is %s (%s)" % (next_player_slack_tags[current_drafter_index][draft_key], replaceUidWithUsername(next_player_slack_tags[current_drafter_index][draft_key], uid_to_username)))
+			if prev_draft_round != current_draft_round:
+				print("Done with round %s after %s's pick" % (prev_draft_round, uid_to_username[uid_draft_order[current_drafter_index]]))
+			#print("New tag to match is %s (%s)" % (next_player_slack_tags[current_drafter_index][draft_key], replaceUidWithUsername(next_player_slack_tags[current_drafter_index][draft_key], uid_to_username)))
 			# check that there aren't a lot of legal tags in between the start and end index.  If so, we probably missed one and should do a re-scan
 			# this might not work?? needs more testing
 			inner_message_tag_count = 0
