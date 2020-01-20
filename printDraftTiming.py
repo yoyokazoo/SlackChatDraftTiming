7ff8fd06ee7839da63bfba34e4875060ebbcb846
yoyokazoo <peter.beckfield@gmail.com>
2020-01-19 18:58:58 -0800

    Grab channels if ID isn't specified.  Grab conversations.

--- printDraftTiming.py
+++ printDraftTiming.py
@@ -5,18 +5,34 @@ from ast import literal_eval
 import slack
 
 parser = argparse.ArgumentParser() 
-parser.add_argument('-token', '--token', help="Slack token of the channel you want to scan", required=True)
+parser.add_argument('-token', '--token', help="Workspace token of the app", required=True)
+parser.add_argument('-channel_id', '--channel_id', help="Channel ID of the channel you want to scan.  If not included, it will list the available channels and ids")
 args = parser.parse_args()
 print(args)
 
 client = slack.WebClient(token=args.token)
 
-print(client.channels_list())
-print(client.conversations_list())
+if(not args.channel_id):
+		print("No channel ID specified.  Here are the available channels:")
+		channel_list = client.channels_list()
+		for channel in channel_list['channels']:
+			print(channel['name'] + " -- " + channel['id'])
+		exit()
 
-response = client.chat_postMessage(
-    channel='CS0FCKUKA',
-    text="Hello world!",
-    user='U106198RM')
-assert response["ok"]
-assert response["message"]["text"] == "Hello world!"
+conversations = None
+while True:
+	if not conversations:
+		conversations = client.conversations_history(channel=args.channel_id)
+	else:
+		conversations = client.conversations_history(
+	  	channel=args.channel_id,
+	  	cursor=conversations['response_metadata']['next_cursor']
+		)
+
+	if not conversations['ok']:
+		break
+
+	print(str(conversations['ok']) + " -- " + str(conversations['has_more']) + " -- " + str(len(conversations['messages'])))
+
+	if not conversations['has_more']:
+		break
